52f8e0dfe1b8db2bb5a443a4a8709873ca5d9d9c
Xabier <xabi.ag.7@gmail.com>
2019-06-12 13:35:35 +0200

    [ADD] Test

--- youtube_api_test.py
+++ youtube_api_test.py
@@ -1,9 +1,11 @@
 from yt_transcript.youtube_transcript_api._api import YouTubeTranscriptApi as yt
+import json 
 
 videoId_with22_subtitles = "uIb1JU9j-Bk"
 videoId_withNone_Subtitles = "QXSDT3v8wsQ"
 try:
-    list = yt.get_transcript(videoId_with22_subtitles, languages=['en'])
-    print(list)
+    lista = yt.get_transcript(videoId_with22_subtitles, languages=['en'])
+    print(lista)
+    
 except yt.CouldNotRetrieveTranscript as err:
     print("ERROR: ", err)
